0df969ecb2188e8f78d959dd8e8fa7043c51d495
linquint <rokaspc@live.com>
2024-06-30 15:07:12 +0300

    Sticker prices loading

--- components/InventoryLoading.tsx
+++ components/InventoryLoading.tsx
@@ -1,11 +1,12 @@
 import { View } from 'react-native';
 import { templates } from 'styles/global';
-import { IInventories, IInventoryGame, ISteamInventoryRes, ISteamProfile } from 'types';
+import { IInventories, IInventoryGame, IItemStickers, ISteamInventoryRes, ISteamProfile } from 'types';
 import Profile from './Profile';
 import Loader from './Loader';
 import { useEffect, useState } from 'react';
 import api from '@utils/api';
 import { helpers } from '@utils/helpers';
+import useStore from 'store';
 
 interface IInventoryLoadingProps {
   profile: ISteamProfile;
@@ -15,6 +16,7 @@ interface IInventoryLoadingProps {
 
 export default function InventoryLoading(props: IInventoryLoadingProps) {
   const $api = new api();
+  const $store = useStore();
   const { profile, games, setInventory } = props;
   const [ loadingMsg, setLoadingMsg ] = useState('');
   const [ loading, setLoading ] = useState(false);
@@ -40,6 +42,7 @@ export default function InventoryLoading(props: IInventoryLoadingProps) {
         appid: +appid,
         items: inv.descriptions.filter(desc => desc.marketable).map(desc => desc.market_hash_name),
       }));
+      const stickersToLoad: string[] = [];
       const pricesRes = await $api.getPrices({ data });
       const finalItems: IInventories = {};
       for (const [ appid, inv ] of Object.entries(inventoryRes)) {
@@ -47,6 +50,12 @@ export default function InventoryLoading(props: IInventoryLoadingProps) {
         finalItems[appid] = [];
         for (const item of inv.descriptions) {
           const price = prices[item.market_hash_name];
+          const stickers: IItemStickers | undefined = appid !== '730' ? undefined : helpers.inv.findStickers(item.descriptions);
+          stickers?.items.forEach(sticker => {
+            if (!stickersToLoad.includes(sticker.longName) && !(sticker.longName in $store.stickerPrices)) {
+              stickersToLoad.push(sticker.longName);
+            }
+          });
           finalItems[appid].push({
             ...item,
             amount: helpers.inv.itemCount(inv.assets, item.classid, item.instanceid),
@@ -54,8 +63,15 @@ export default function InventoryLoading(props: IInventoryLoadingProps) {
               ...(price || { found: false }),
               difference: price ? helpers.inv.priceDiff(price) : 0,
             },
+            stickers,
           });
         }
+      }
+
+      if (stickersToLoad.length > 0) {
+        setLoadingMsg('Loading stickers and patches');
+        const stickerPricesRes = await $api.getStickerPrices(stickersToLoad);
+        Object.entries(stickerPricesRes).map(([ name, { price } ]) => $store.stickerPrices[name] = price);
       }
       setInventory(finalItems);
     }
--- zustand/store.ts
+++ zustand/store.ts
@@ -4,6 +4,7 @@ import { create } from 'zustand';
 interface IStore {
   games: IInventoryGame[];
   inventory: IInventories;
+  stickerPrices: { [hash: string]: number }
   setGames: (games: IInventoryGame[]) => void;
   setInventory: (inv: IInventories) => void;
 }
@@ -11,6 +12,7 @@ interface IStore {
 const useStore = create<IStore>()(set => ({
   games: [],
   inventory: {},
+  stickerPrices: {},
   setGames: (games: IInventoryGame[]) => set(() => ({ games })),
   setInventory: (inv: IInventories) => set(() => ({ inventory: inv })),
 }));
