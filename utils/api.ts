34ae55a22d28b4537d17cd8a099c8ede59c2aedb
linquint <rokaspc@live.com>
2024-06-30 15:07:27 +0300

    Sticker price API route

--- utils/api.ts
+++ utils/api.ts
@@ -10,38 +10,25 @@ export default class api {
   }
 
   public async getRates(): Promise<IExchangeRate[]> {
-
     console.log('calling getRates');
-
-    try {
-      const ratesRes = await this.axiosInstance.get<IExchangeRate[]>('/rates');
-      return ratesRes.data;
-    } catch {
-      console.error('Failed to get rates.');
-    }
-    return [];
+    const ratesRes = await this.axiosInstance.get<IExchangeRate[]>('/rates');
+    return ratesRes.data;
   }
 
   public async getInventoryGames(): Promise<IInventoryGame[]> {
-
     console.log('calling getInventoryGames');
-
     const gamesRes = await this.axiosInstance.get<IInventoryGame[]>('/games/inventory');
     return gamesRes.data;
   }
 
   public async getMusicKits(): Promise<IMusicKitsRes> {
-
     console.log('calling getMusicKits');
-
     const musicKits = await this.axiosInstance.get<IMusicKitsRes>('/musickits');
     return musicKits.data;
   }
 
   public async findSteamIdFromVanity(str: string): Promise<string | null> {
-
     console.log('calling findSteamIdFromVanity');
-
     const vanityRes = await this.axiosInstance.get<IVanityRes>(`https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${process.env.EXPO_PUBLIC_STEAM_KEY}&vanityurl=${str}`);
     if (vanityRes.data.response.success) {
       return vanityRes.data.response.steamid;
@@ -50,9 +37,7 @@ export default class api {
   }
 
   public async findSteamProfile(str: string): Promise<ISteamUser | null> {
-
     console.log('calling findSteamProfile');
-
     const profileRes = await this.axiosInstance.get<ISteamUserRes>(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.EXPO_PUBLIC_STEAM_KEY}&steamids=${str}`);
     if (profileRes.data.response.players.length > 0) {
       return profileRes.data.response.players[0];
@@ -61,9 +46,7 @@ export default class api {
   }
 
   public async batchSteamProfiles(ids: string[]): Promise<ISteamUser[]> {
-
     console.log('calling batchSteamProfiles');
-
     const profilesRes = await this.axiosInstance.get<ISteamUserRes>(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.EXPO_PUBLIC_STEAM_KEY}&steamids=${ids.join(',')}`);
     return profilesRes.data.response.players;
   }
@@ -71,6 +54,12 @@ export default class api {
   public async getPrices(body: IPricesReq): Promise<IPricesRes> {
     console.log('calling getPrices');
     const pricesRes = await this.axiosInstance.post<IPricesRes>('https://linquint.dev/api/prices', body);
+    return pricesRes.data;
+  }
+
+  public async getStickerPrices(stickers: string[]): Promise<{ [hash: string]: { price: number } }> {
+    console.log('calling getStickerPrices');
+    const pricesRes = await this.axiosInstance.post<{ [hash: string]: { price: number } }>('https://linquint.dev/api/stickers', stickers);
     return pricesRes.data;
   }
 
